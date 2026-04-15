962090846fa7286bc0f7bf4bc0a846957f8e88cb
Rodrigo Grigera <rodrigogrigera@gmail.com>
2026-04-14 21:01:45 -0300

    Componente buscar producto

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,16 @@
+import { SearchProducts } from "./componentes/BuscarProducto/BuscarProducto";
 
 
 function App() {
-  
+   const handleSearch = (buscar: string) => {
+    console.log("Buscando:", buscar);
+  };
 
   return (
     <><h1>esto es dev...sapeeeeeee</h1>
-         </>
+      <h3>Productos</h3>
+      <SearchProducts onSearch={handleSearch} />
+    </>
   )
 }
 
---
+++ src/componentes/BuscarProducto/BuscarProducto.tsx
@@ -0,0 +1,27 @@
+import { useState } from "react";
+
+type SearchProps = {
+  onSearch: (buscar: string) => void;
+};
+
+export function SearchProducts({onSearch}:SearchProps){
+    const [buscar, setBuscar] = useState<string>('');
+
+     const handleSearch = () => {
+        onSearch(buscar);
+        setBuscar('');
+  };
+    
+    return (
+        <form  onSubmit={(e) => {
+        e.preventDefault();
+        handleSearch();
+        }}>
+            <input type="text"
+             placeholder="Buscar producto"
+             value={buscar} 
+             onChange={(e) => setBuscar(e.target.value)}/>
+             <button  type="submit">Buscar</button>
+        </form>
+    )
+}
