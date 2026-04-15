836a4f86d4ff8e77ff353620a1c956f4e1ea8361
Rodrigo Grigera <rodrigogrigera@gmail.com>
2026-04-15 18:56:32 -0300

    modificaciones , en css y importaciones de componentes

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,8 @@
 import { SearchProducts } from "./componentes/BuscarProducto/BuscarProducto";
-
+import{BrowserRouter,Routes, Route} from 'react-router-dom';
+import Navbar from "./componentes/nav-bar/Navbar";
+import Login from "./componentes/Login/Login";  
+import Footer from "./componentes/Footer/Footer";
 
 function App() {
    const handleSearch = (buscar: string) => {
@@ -7,9 +10,27 @@ function App() {
   };
 
   return (
-    <><h1>esto es dev...sapeeeeeee</h1>
-      <h3>Productos</h3>
-      <SearchProducts onSearch={handleSearch} />
+    <>
+    <BrowserRouter>
+    <Navbar/>
+
+    <main className="conte-main">
+
+        <Routes>
+          <Route path="/" element={
+        <div>
+        <h3>Productos</h3>
+        <SearchProducts onSearch={handleSearch} />
+        </div>
+          }/>
+          <Route path="/login" element={<Login/>}/>
+          <Route path="/home" element={<h1>Bienvenido 👋</h1>} />
+        </Routes>
+
+    </main>
+        <Footer/>
+    </BrowserRouter>
+
     </>
   )
 }
