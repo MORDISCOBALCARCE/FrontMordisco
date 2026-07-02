c943a47d1e596fb9211705528a5fa5c2580b853f
Rodrigo Grigera <rodrigogrigera@gmail.com>
2026-07-01 21:31:43 -0300

    Modificacion en render carrito

--- src/features/pedidoPorCliente/CardClienteCarrito.tsx
+++ src/features/pedidoPorCliente/CardClienteCarrito.tsx
@@ -12,13 +12,13 @@ export function CardClienteCarrito({ pedido }: Props) {
         <>
         
             <div className="cont-principal">
-                <p className="p-cont">{pedido.detalles.map((p) => <div className="cont-div">
+                <div className="p-cont">{pedido.detalles.map((p) => <div className="cont-div" key={p.id_detalle}>
                     <img src={`${url_imagen}/${p.producto.imagen}`} alt={`${p.producto.nombre}`} style={{ width: '50px', height: '50px' }} />
                     <p>Cantidad: {p.cantidad}</p>
                     <p>Producto: {p.producto.nombre}</p>
                     <p>Fecha: {pedido.fecha_pedido}</p>
                     <p>Estado: {pedido.estado === 'entregado' ? 'listo' : 'pendiente'}</p>
-                </div>)}</p>
+                </div>)}</div>
                     <p>Total Pagar: {pedido.total}</p>
             </div>
         </>
